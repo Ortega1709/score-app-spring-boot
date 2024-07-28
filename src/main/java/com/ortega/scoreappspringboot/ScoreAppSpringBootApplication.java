ab579e5450266cd240f3a24aee068b6611c0a096
ortega <kabweortega@gmail.com>
2024-07-28 17:21:43 +0200

    Initial commit

---
+++ src/main/java/com/ortega/scoreappspringboot/ScoreAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ortega.scoreappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ortega/scoreappspringboot/ScoreAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.scoreappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
